065f4e1a75221deb582c3f0dc804a07adae4bace
HummyMolecula <dragonitdeveloper@gmail.com>
2022-02-19 19:15:09 +0200

    first commit

---
+++ lists.py
@@ -0,0 +1,6 @@
+drinks = ["coffe", "soda", "tea", "juice", "lemonade"]
+dinner = ["pizza", "roll", "hotdog", "hamburger", "sandwich"]
+dessert = ["cake", "biscuit", "ice cream", "fruit"]
+
+food = [drinks, dinner, dessert]
+print(food)
