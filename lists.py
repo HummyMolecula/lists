df7df1dcfa6d3ab9477a3ef28de54e9f7d143110
HummyMolecula <dragonitdeveloper@gmail.com>
2022-02-22 16:08:31 +0200

    lists update

--- lists.py
+++ lists.py
@@ -1,6 +1,21 @@
+lenght = int(input("Enter length: "))
+
 drinks = ["coffe", "soda", "tea", "juice", "lemonade"]
 dinner = ["pizza", "roll", "hotdog", "hamburger", "sandwich"]
 dessert = ["cake", "biscuit", "ice cream", "fruit"]
+food = []
 
-food = [drinks, dinner, dessert]
+i = 0
+for n in range(3):
+    while i < lenght:
+        if n == 0:
+            food.append(drinks[i])
+        elif n == 1:
+            food.append(dinner[i])
+        else:
+            food.append(dessert[i])
+
+        i += 1
+    i = 0
+
 print(food)
